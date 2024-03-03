2f9cf6a35dbe7f0d386da47640bc7174e56e8e1b
Amauri <amaurimelojunior@gmail.com>
2024-03-03 12:25:37 -0300

    Composer initialization and initial dependencies

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use DI\Bridge\Slim\Bridge;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = Bridge::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
